531faf67c507f43f60f4b7a2db9bff7983ad1bfa
tunglin29 <yentunglin29@gmail.com>
2024-09-30 18:58:33 -0700

    Add url to localhost

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use('/users', mealPlansRoutes);
 app.use('/meals', mealsRoutes);
 
 // Server setup
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
