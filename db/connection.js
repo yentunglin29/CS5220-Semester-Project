cbd4d337bb0ddd56e0acd4939ba3b8c9f19d2a83
tong0929 <yentunglin29@gmail.com>
2024-11-13 09:09:26 -0800

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,22 @@
-// future mongoDB use
+// function to mock a database connection
+import mongoose from 'mongoose';
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_ADDRESS = process.env.DB_ADDRESS;
+const DB_NAME = process.env.DB_NAME;
+
+import User from "../api/models/user.js";
+import Mealplan from "../api/models/mealplan.js";
+
+const connect = async () => {
+    try {
+        const MONGO_URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_ADDRESS}/?retryWrites=true&w=majority&appName=Cluster0`;
+        await mongoose.connect(MONGO_URI, { dbName: DB_NAME });
+        console.log('Connected to Mongo');
+    } catch (error) {
+        console.error(`Error connecting to Mongo: ${error.message}`);
+    }
+};
+
+export default { connect };
