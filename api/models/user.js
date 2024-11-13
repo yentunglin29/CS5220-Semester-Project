51f1096ce08dcdd4e113a17bb7709d776afe44ed
tong0929 <yentunglin29@gmail.com>
2024-11-13 11:03:04 -0800

    Remove pre hook

--- api/models/user.js
+++ api/models/user.js
@@ -32,15 +32,9 @@ UserSchema.virtual('mealplans', {
     foreignField: 'user_id'
 });
 
-// Pre-save hook to hash the password before saving
-UserSchema.pre('save', async function (next) {
-    await hashUserPassword(this);
-    next();
-});
-
-// Post-save hook to log user creation
-UserSchema.post('save', function (doc) {
-    console.log(`User ${doc.username} has been created`);
+// Post-save hook to log when a meal plan is created
+MealPlanSchema.post('save', function (doc) {
+    console.log(`Meal Plan for user ${doc.user_id} for week ${doc.week} has been created`);
 });
 
 const User = mongoose.model('User', UserSchema);
